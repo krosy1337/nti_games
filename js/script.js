0e6fd4779440dd858d535a85e623403c06950358
KROSY <45696255+krosy1337@users.noreply.github.com>
2021-04-18 13:21:46 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,35 +1,44 @@
 const mainGameButtons = document.querySelectorAll('.main__game-button_o')
 
-mainGameButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		const containerBody = button.closest('.game').querySelector('.game__body')
+if (mainGameButtons) {
+	mainGameButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			const containerBody = button.closest('.game').querySelector('.game__body')
 
-		if (containerBody.classList.contains('active')) {
-			containerBody.style.maxHeight = null
-			containerBody.classList.remove('active')
-		} else {
-			containerBody.style.maxHeight = containerBody.scrollHeight + 'px'
-			containerBody.classList.add('active')
-		}
+			if (containerBody.classList.contains('active')) {
+				containerBody.style.maxHeight = null
+				containerBody.classList.remove('active')
+			} else {
+				containerBody.style.maxHeight = containerBody.scrollHeight + 'px'
+				containerBody.classList.add('active')
+			}
 
-		button.classList.toggle('active')
+			button.classList.toggle('active')
+		})
 	})
-})
+}
+
+
 
 const sideBar = document.querySelector('.side')
 const burger = document.querySelector('.header__burger')
 const overlay = document.querySelector('.overlay')
 
-overlay.addEventListener('click', () => {
-	sideBar.classList.remove('active')
-	document.body.classList.remove('lock')
-	overlay.classList.remove('active')
-	burger.classList.remove('active')
-})
+if (overlay) {
+	overlay.addEventListener('click', () => {
+		sideBar.classList.remove('active')
+		document.body.classList.remove('lock')
+		overlay.classList.remove('active')
+		burger.classList.remove('active')
+	})
+}
 
-burger.addEventListener('click', () => {
-	sideBar.classList.toggle('active')
-	document.body.classList.toggle('lock')
-	overlay.classList.toggle('active')
-	burger.classList.toggle('active')
-})
+if (burger) {
+	burger.addEventListener('click', () => {
+		sideBar.classList.toggle('active')
+		document.body.classList.toggle('lock')
+		overlay.classList.toggle('active')
+		burger.classList.toggle('active')
+	})
+}
+
