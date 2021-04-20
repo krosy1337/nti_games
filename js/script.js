41857970e9152f7581932bf4815b6d1023eee8c0
KROSY <45696255+krosy1337@users.noreply.github.com>
2021-04-20 19:12:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,15 +22,30 @@ if (mainGameButtons) {
 const switchButton = document.querySelector('.theme-switch')
 
 function initialState(themeName) {
+	if (switchButton) {
+		if (themeName === 'light-theme') {
+			switchButton.checked = false
+		} else {
+			switchButton.checked = true
+		}	
+	}
 	localStorage.setItem('theme', themeName)
 	document.documentElement.className = themeName
 }
 
-initialState('light-theme')
+function initTheme() {
+	if (localStorage.getItem('theme')) {
+		initialState(localStorage.getItem('theme'))
+		return
+	}
+	initialState('light-theme')
+}
+
+initTheme()
 
 if (switchButton) {
 	switchButton.addEventListener('change', () => {
-		if (switchButton.checked) {
+		if (localStorage.getItem('theme') === 'light-theme') {
 			initialState('dark-theme')
 			return
 		}
