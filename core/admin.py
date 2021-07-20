3901509304b8cff565a28552c70f277f8afb72eb
KROSY <45696255+krosy1337@users.noreply.github.com>
2021-07-20 17:16:57 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import TalantUser, CsResult, DotaResult
 
 
-admin.site.register(TalantUser)
 admin.site.register(CsResult)
 admin.site.register(DotaResult)
+
+@admin.register(TalantUser)
+class TalantUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cs_result', 'dota_result')
